191c081594235589196ee7e4f1f86b37a5df48ea
xuchaoxin <838808930@qq.com>
2021-04-11 14:23:57 +0800

    test modify

--- python_study_Demos/list/testlist.py
+++ python_study_Demos/list/testlist.py
@@ -4,7 +4,7 @@ Version: 2.0
 Author: xuchaoxin
 Date: 2021-04-08 22:55:30
 LastEditors: xuchaoxin
-LastEditTime: 2021-04-11 12:44:54
+LastEditTime: 2021-04-11 14:23:25
 '''
 """ if else list comprehesion """
 a = [i for i in range(10) if i % 2 == 0]
@@ -12,3 +12,4 @@ print(a)
 a = [i if i % 2 == 0 else "pass" for i in range(10)]
 a = [i if i % 2 == 0 else 'qi' for i in range(10)]
 print(a)
+# test git
